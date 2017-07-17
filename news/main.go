214cdb06a08e6508beb073a1b86d07f8e13aa6b1
Zhanliang Liu <zhanliangliu@qq.com>
2017-07-17 16:53:26 +0800

    get news feed

---
+++ news/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"github.com/mmcdole/gofeed"
+	"log"
+)
+
+func main() {
+	//https://news.google.com/news/headlines?ned=us&hl=en
+	url := "https://www.yahoo.com/news/rss/mostviewed"
+	fp := gofeed.NewParser()
+	feed, err := fp.ParseURL(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(feed.Title)
+	for _, item := range feed.Items {
+		log.Println(item.Title, item.Link)
+	}
+}
