30fff30e49b262d318531f9fa390611107d5b8ae
liuzl <zhanliangliu@qq.com>
2017-08-01 22:25:45 +0800

    parsetime and timezone

---
+++ ptime/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/tkuchiki/parsetime"
+	"github.com/ttacon/libphonenumber"
+	"log"
+)
+
+var (
+	loc = flag.String("loc", "US", "two letter country code")
+	str = flag.String("str", "", "time to parse")
+)
+
+func main() {
+	flag.Parse()
+	cc := libphonenumber.GetCountryCodeForRegion(*loc)
+	tz, ok := libphonenumber.CountryCodeToTimeZones[cc]
+	if ok {
+		for _, t := range tz {
+			fmt.Println(t)
+			p, err := parsetime.NewParseTime(t)
+			if err != nil {
+				log.Fatal(err)
+			}
+			t, err := p.Parse(*str)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(t)
+		}
+	}
+}
