8b4acd767bb86c70ea09f77b5d02fbcf94eb2c95
liuzhanliang <liuzhanliang@baidu.com>
2017-08-13 07:46:05 +0800

    httpdump

--- httpdump/main.go
+++ httpdump/main.go
@@ -12,6 +12,7 @@ func main() {
 	const body = "Go is a general-purpose language designed with systems programming in mind."
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Date", "Wed, 19 Jul 1972 19:00:00 GMT")
+		w.Header().Set("Other", "Any headers here")
 		fmt.Fprintln(w, body)
 	}))
 	defer ts.Close()
