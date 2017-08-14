42816c493da3d03a30c17b98dacec359aa50806d
liuzhanliang <liuzhanliang@baidu.com>
2017-08-14 11:47:17 +0800

    匿名字段

---
+++ struct/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Animal struct {
+	Name string `json:"name"`
+}
+
+type Dog struct {
+	*Animal
+	Owner string `json:"owner"`
+}
+
+func main() {
+	a := &Animal{Name: "this is an animal"}
+	b, _ := json.Marshal(a)
+	fmt.Println(string(b))
+
+	d := &Dog{a, "this is the owner name of this dog"}
+	b, _ = json.Marshal(d)
+	fmt.Println(string(b))
+}
