ce39898c00078cc2d8a637fa2f2f0756a842c707
liuzhanliang <liuzhanliang@baidu.com>
2017-11-05 17:13:43 +0800

    save work process

--- gzfile/main.go
+++ gzfile/main.go
@@ -5,10 +5,9 @@ import (
 	"compress/gzip"
 	"encoding/json"
 	"flag"
-	"fmt"
+	"github.com/golang/glog"
 	"github.com/ttacon/libphonenumber"
 	"io"
-	"log"
 	"os"
 	"strings"
 )
@@ -19,21 +18,25 @@ type Contact struct {
 }
 
 type Item struct {
+	IMEI        string    `json:"a1"`
+	Phone       string    `json:"a2"`
+	Loc         string    `json:"a5"`
 	CountryCode string    `json:"a3"`
 	Contacts    []Contact `json:"b"`
 }
 
 func main() {
+	defer glog.Flush()
 	filename := flag.String("file", "./file.txt.gz", "file to read")
 	flag.Parse()
 	file, err := os.Open(*filename)
 	if err != nil {
-		log.Fatal(err)
+		glog.Fatal(err)
 	}
 	defer file.Close()
 	gr, err := gzip.NewReader(file)
 	if err != nil {
-		log.Fatal(err)
+		glog.Fatal(err)
 	}
 	br := bufio.NewReader(gr)
 	i := 0
@@ -46,16 +49,24 @@ func main() {
 		i++
 		err = json.Unmarshal(line, &item)
 		if err != nil {
-			log.Println(err)
+			glog.Error(err)
+			continue
 		}
 		cc := strings.ToUpper(item.CountryCode)
+		glog.Info("loc:", item.Loc, "\tIMEI:", item.IMEI, "\tPhone:", item.Phone, "\tcc:", cc)
 		for _, contact := range item.Contacts {
-			fmt.Println(contact.Phone)
 			number, err := libphonenumber.Parse(contact.Phone, cc)
 			if err != nil {
-				log.Println("ERROR", err)
+				//		glog.Error(contact.Phone, err)
+				continue
 			}
-			fmt.Println(number, contact.Name)
+
+			if !libphonenumber.IsValidNumber(number) {
+				//		glog.Error(contact.Phone, "|", cc, "|", contact.Name, " invalid number")
+				continue
+			}
+
+			//	glog.Info(contact.Phone, "|", cc, "|", contact.Name, " ", number)
 		}
 	}
 }
