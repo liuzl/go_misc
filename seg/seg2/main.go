5f309fe97c486df9a2975d93bb2e75fbc8f819ac
liuzhanliang <liuzhanliang@baidu.com>
2017-11-13 16:46:31 +0800

    seg test

---
+++ seg/seg2/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/blevesearch/segment"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+	br := bufio.NewReader(os.Stdin)
+	for {
+		line, c := br.ReadBytes('\n')
+		if c == io.EOF {
+			break
+		}
+		fmt.Println(string(line[:len(line)-1]))
+		seg1 := segment.NewSegmenterDirect(line)
+		for seg1.Segment() {
+			text := seg1.Text()
+			if strings.TrimSpace(text) == "" {
+				continue
+			}
+			label := seg1.Type()
+			fmt.Print(text, "\\", label, " ")
+		}
+		fmt.Println("\n")
+	}
+}
