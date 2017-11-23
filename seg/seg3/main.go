e8e5479b49fee2e99999f83b87b8d575e7a0a6b7
liuzhanliang <liuzhanliang@baidu.com>
2017-11-23 21:41:38 +0800

    unicode text types

--- seg/seg3/main.go
+++ seg/seg3/main.go
@@ -9,6 +9,15 @@ import (
 	"strings"
 )
 
+// Word Types
+const (
+	None = iota
+	Number
+	Letter
+	Kana
+	Ideo
+)
+
 func main() {
 	br := bufio.NewReader(os.Stdin)
 	for {
@@ -28,4 +37,6 @@ func main() {
 		}
 		fmt.Println("\n")
 	}
+	fmt.Println(None, Number, Letter, Kana, Ideo)
+	fmt.Println("None, Number, Letter, Kana, Ideo")
 }
