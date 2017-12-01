46f41366bf8378ac11be31eede527b65a1200224
liuzhanliang <liuzhanliang@baidu.com>
2017-12-01 14:30:33 +0800

    pointer test

--- pointer/main.go
+++ pointer/main.go
@@ -9,8 +9,16 @@ func main() {
 	println(u.Name)
 	Modify(u)
 	println(u.Name)
+	arr := []int{}
+	println(arr)
+	M(&arr)
+	println(arr)
 }
 
 func Modify(u User) {
 	u.Name = "Duncan"
+}
+
+func M(arr *[]int) {
+	*arr = append(*arr, 1)
 }
