1ced8ae04fefb2e82cd973d012d95e3ca23013fe
liuzhanliang <liuzhanliang@baidu.com>
2018-02-04 22:13:42 +0800

    unexpected EOF debugging for phonenumbers

--- phonenumber/liuzl/main.go
+++ phonenumber/liuzl/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"bytes"
+	"compress/gzip"
+	"encoding/base64"
 	"fmt"
 	"github.com/liuzl/phonenumbers"
+	"io/ioutil"
 )
 
 func main() {
 	fmt.Println("vim-go")
 	fmt.Println(phonenumbers.CarriersPb)
+	data, err := base64.StdEncoding.DecodeString(phonenumbers.CarriersPb)
+	fmt.Println(data, err)
+	reader, err := gzip.NewReader(bytes.NewReader(data))
+	fmt.Println(reader, err)
+	pbBytes, err := ioutil.ReadAll(reader)
+	fmt.Println(pbBytes, err)
 }
