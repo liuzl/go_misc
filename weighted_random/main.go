f5cacf2a228158381b2d023b22973f276cd1a4e1
liuzhanliang <zhanliangliu@qq.com>
2018-04-09 16:57:49 +0800

    add binary search version

--- weighted_random/main.go
+++ weighted_random/main.go
@@ -37,8 +37,39 @@ func WeightedRandomS2(weights []float32) int {
 	return len(weights) - 1
 }
 
+func WeightedRandomS3(weights []float32) int {
+	n := len(weights)
+	if n == 0 {
+		return 0
+	}
+	cdf := make([]float32, n)
+	var sum float32 = 0.0
+	for i, w := range weights {
+		if i > 0 {
+			cdf[i] = cdf[i-1] + w
+		} else {
+			cdf[i] = w
+		}
+		sum += w
+	}
+	r := rand.Float32() * sum
+	var l, h int = 0, n - 1
+	for l <= h {
+		m := l + (h-l)/2
+		if r <= cdf[m] {
+			if m == 0 || (m > 0 && r > cdf[m-1]) {
+				return m
+			}
+			h = m - 1
+		} else {
+			l = m + 1
+		}
+	}
+	return -1
+}
+
 func main() {
 	for i := 0; i < 100; i++ {
-		fmt.Println(WeightedRandomS2([]float32{0.1, 0.3, 0.6}))
+		fmt.Println(WeightedRandomS3([]float32{0.1, 0.3, 0.2}))
 	}
 }
