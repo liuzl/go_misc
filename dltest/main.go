12482abec7d7c31e4cff72b20d0942c575963193
liuzhanliang <zhanliangliu@qq.com>
2018-05-07 17:57:59 +0800

    test DownloadUrl

--- dltest/main.go
+++ dltest/main.go
@@ -11,6 +11,7 @@ import (
 // WriteFile writes data to file at filePath
 func WriteFile(filePath, fileName string, data []byte) {
 	path := filepath.Join(filePath, fileName)
+	fmt.Println(path)
 	err := ioutil.WriteFile(path, data, os.FileMode(0664))
 	if err != nil {
 		fmt.Println(err)
@@ -18,8 +19,7 @@ func WriteFile(filePath, fileName string, data []byte) {
 }
 
 func main() {
-	req := &dl.HttpRequest{Url: "https://instagram.fcgk6-1.fna.fbcdn.net/vp/6af9dfe77a72a523f5990c74a8c537fb/5AC4DA8F/t50.2886-16/29911547_158978848119097_7202585723608760320_n.mp4"}
-	resp := dl.Download(req)
+	resp := dl.DownloadUrl("https://instagram.fcgk6-1.fna.fbcdn.net/vp/6af9dfe77a72a523f5990c74a8c537fb/5AC4DA8F/t50.2886-16/29911547_158978848119097_7202585723608760320_n.mp4")
 	if resp.Error != nil {
 		fmt.Printf("error fetching: %+v\n", resp.Error)
 		return
