7450571a5cf0b29b14d1560b0a624677fb6059b7
liuzhanliang <zhanliangliu@qq.com>
2018-05-07 17:39:07 +0800

    ReverseOrderEncode

--- str/main.go
+++ str/main.go
@@ -5,6 +5,18 @@ import (
 	"strings"
 )
 
+func ReverseOrderEncode(s string) string {
+	var buf []rune
+	for _, c := range s {
+		if c >= '0' && c <= '9' {
+			buf = append(buf, '9'-c+'0')
+		} else {
+			buf = append(buf, c)
+		}
+	}
+	return string(buf)
+}
+
 func main() {
 	fmt.Println("vim-go")
 	dot := "\u00B7"
@@ -14,4 +26,9 @@ func main() {
 	l := strings.ToLower(str)
 	u := strings.ToUpper(l)
 	fmt.Println(str, l, u)
+
+	s := "2018050716"
+	fmt.Println(s)
+	fmt.Println(ReverseOrderEncode(s))
+	fmt.Println(ReverseOrderEncode("hello90"))
 }
