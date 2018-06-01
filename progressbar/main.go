ba219982ab444d44ce0a0882920b1477b242e4c8
liuzhanliang <zhanliangliu@qq.com>
2018-06-01 10:02:02 +0800

    goutil.FileLineCount

--- progressbar/main.go
+++ progressbar/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	//"gopkg.in/cheggaaa/pb.v1"
+	"fmt"
 	"github.com/cheggaaa/pb"
+	"github.com/liuzl/goutil"
 	"time"
 )
 
 func main() {
+	fmt.Println(goutil.FileLineCount("main.go"))
 	count := 100000
 	bar := pb.StartNew(count)
 	for i := 0; i < count; i++ {
