06ec00996a656ee499c17e30901462c92c8c6248
Zhanliang Liu <zhanliangliu@qq.com>
2018-08-17 11:53:14 +0800

    update structhash

--- structhash/main.go
+++ structhash/main.go
@@ -10,9 +10,13 @@ func main() {
 	fmt.Println("vim-go")
 
 	var item1, item2 interface{}
+	var item3 map[string]string
+	var item4 map[uint64]string
 	item1 = "hello world"
-	item2 = []string{"hello world"}
 	h1, _ := hashstructure.Hash(item1, nil)
 	h2, _ := hashstructure.Hash("hello world", nil)
-	fmt.Println(h1, h2, item2)
+	h3, _ := hashstructure.Hash(item2, nil)
+	h4, _ := hashstructure.Hash(item3, nil)
+	h5, _ := hashstructure.Hash(item4, nil)
+	fmt.Println(h1, h2, item2, h3, h4, h5)
 }
