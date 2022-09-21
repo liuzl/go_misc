d89656ddff7397aaf6fc585257da112320065901
Zhanliang Liu <liang@zliu.org>
2022-09-21 17:56:22 +0800

    translation server

--- bertserver/go.mod
+++ bertserver/go.mod
@@ -3,9 +3,9 @@ module bertserver
 go 1.19
 
 require (
-	github.com/joho/godotenv v1.4.0
+	github.com/golang/glog v1.0.0
 	github.com/nlpodyssey/cybertron v0.1.0
-	github.com/rs/zerolog v1.28.0
+	zliu.org/goutil v0.0.0-20220902023633-6fcbc3a43c89
 )
 
 require (
@@ -16,12 +16,13 @@ require (
 	github.com/dlclark/regexp2 v1.7.0 // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/golang/glog v1.0.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/flatbuffers v2.0.6+incompatible // indirect
+	github.com/justinas/alice v1.2.0 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/liuzl/filestore v0.0.0-20200229104338-5ea723a6a528 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/nlpodyssey/gopickle v0.1.0 // indirect
@@ -29,8 +30,10 @@ require (
 	github.com/nlpodyssey/spago v1.0.1 // indirect
 	github.com/nlpodyssey/spago/embeddings/store/diskstore v0.0.0-20220801114813-013d65be77fb // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rs/xid v1.4.0 // indirect
+	github.com/rs/zerolog v1.28.0 // indirect
 	go.opencensus.io v0.23.0 // indirect
-	golang.org/x/net v0.0.0-20220728211354-c7608f3a8462 // indirect
+	golang.org/x/net v0.0.0-20220826154423-83b083e8dc8b // indirect
 	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
 	golang.org/x/sys v0.0.0-20220731174439-a90be440212d // indirect
 	golang.org/x/text v0.3.7 // indirect
--- bertserver/main.go
+++ bertserver/main.go
@@ -2,45 +2,60 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"os"
+	"flag"
+	"net/http"
+	"strings"
 	"time"
 
+	"github.com/golang/glog"
 	"github.com/nlpodyssey/cybertron/pkg/tasks"
 	"github.com/nlpodyssey/cybertron/pkg/tasks/text2text"
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
+	"zliu.org/goutil/rest"
 )
 
 var (
-	modelsDir = "models"
-	modelName = "Helsinki-NLP/opus-mt-en-zh"
+	addr      = flag.String("addr", ":8300", "bind address")
+	modelsDir = flag.String("dir", "models", "the models dir")
+	modelName = flag.String("name", "Helsinki-NLP/opus-mt-en-zh", "the translation model name")
 )
 
-func main() {
-	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+type TranslationResult struct {
+	Input  string  `json:"input"`
+	Output string  `json:"output"`
+	Time   float64 `json:"time"`
+}
 
-	m, err := tasks.Load[text2text.Interface](&tasks.Config{ModelsDir: modelsDir, ModelName: modelName})
+func main() {
+	flag.Parse()
+	m, err := tasks.Load[text2text.Interface](&tasks.Config{ModelsDir: *modelsDir, ModelName: *modelName})
 	if err != nil {
-		log.Fatal().Err(err).Send()
+		glog.Fatal(err)
 	}
 	defer tasks.Finalize(m)
 
 	opts := text2text.DefaultOptions()
 
-	fn := func(text string) error {
+	http.Handle("/translate", rest.WithLog(func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		text := strings.TrimSpace(r.FormValue("text"))
+		if text == "" {
+			rest.MustEncode(w, &rest.RestMessage{Status: "fail", Message: "input text is empty"})
+			return
+		}
 		start := time.Now()
 		result, err := m.Generate(context.Background(), text, opts)
 		if err != nil {
-			return err
+			rest.MustEncode(w, &rest.RestMessage{Status: "fail", Message: err.Error()})
+			glog.Error(err)
+			return
 		}
-		fmt.Println(time.Since(start).Seconds())
-		fmt.Println(result.Texts[0])
-		return nil
-	}
-
-	err = ForEachInput(os.Stdin, fn)
-	if err != nil {
-		log.Fatal().Err(err)
-	}
+		ret := &TranslationResult{
+			text,
+			result.Texts[0],
+			time.Since(start).Seconds(),
+		}
+		rest.MustEncode(w, &rest.RestMessage{Status: "ok", Message: ret})
+	}))
+	glog.Info("server listen on ", *addr)
+	glog.Error(http.ListenAndServe(*addr, nil))
 }
