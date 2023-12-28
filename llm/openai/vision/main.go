aa99f4e8f6839654c8e5da9e9130ee9ac24decaf
liuzl <liang@zliu.org>
2023-12-28 21:24:17 +0800

    openai sdk, vision

---
+++ llm/openai/vision/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
+	openai "github.com/sashabaranov/go-openai"
+)
+
+func main() {
+	config := openai.DefaultConfig(os.Getenv("OPENAI_API_KEY"))
+	config.BaseURL = os.Getenv("OPENAI_API_BASE")
+	client := openai.NewClientWithConfig(config)
+	url := "https://images.unsplash.com/5/unsplash-kitsune-3.jpg?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8M3wxMDA0NjU5fHxlbnwwfHx8fHw%3D"
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: "gemini-pro-vision",
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role: openai.ChatMessageRoleUser,
+					MultiContent: []openai.ChatMessagePart{
+						{Type: openai.ChatMessagePartTypeText, Text: "Please describe this image"},
+						{Type: openai.ChatMessagePartTypeImageURL, ImageURL: &openai.ChatMessageImageURL{URL: url}},
+					},
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return
+	}
+
+	fmt.Println(resp.Choices[0].Message.Content)
+}
