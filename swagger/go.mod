0ca0c4a306cf8fb7621f5917de06526eb11d4fd3
liuzl <liang@zliu.org>
2024-01-10 17:02:49 +0800

    golang swagger

---
+++ swagger/go.mod
@@ -0,0 +1,31 @@
+module swagger_test
+
+go 1.21.5
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/golang/glog v1.2.0
+	github.com/swaggo/http-swagger/v2 v2.0.2
+	github.com/swaggo/swag v1.16.2
+	zliu.org/goutil v0.0.0-20240101042343-3ed76167ff58
+)
+
+require (
+	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.20.0 // indirect
+	github.com/go-openapi/spec v0.20.6 // indirect
+	github.com/go-openapi/swag v0.19.15 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/justinas/alice v1.2.0 // indirect
+	github.com/liuzl/filestore v0.0.0-20200229104338-5ea723a6a528 // indirect
+	github.com/mailru/easyjson v0.7.6 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/rs/xid v1.4.0 // indirect
+	github.com/rs/zerolog v1.28.0 // indirect
+	github.com/swaggo/files/v2 v2.0.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/tools v0.7.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ swagger/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+
+	_ "swagger_test/docs"
+
+	"github.com/go-chi/chi"
+	"github.com/golang/glog"
+	httpswagger "github.com/swaggo/http-swagger/v2"
+	"zliu.org/goutil/rest"
+)
+
+// echo request godoc
+// @Summary echo back the http raw request
+// @Description echo
+// @Success 200 {string} string "Raw request"
+// @Failure 400 {string} string "Bad request"
+// @Failure 404 {string} string "Not found"
+// @Failure 500 {string} string "Internal server error"
+// @Router /echo [post] "echo"
+func echo(w http.ResponseWriter, r *http.Request) {
+	dump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		rest.ErrInternalServer(w, fmt.Sprintf("dump err, %v", err))
+		return
+	}
+	glog.Info(string(dump))
+	w.Write(dump)
+}
+
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server Petstore server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host 127.0.0.1:1323
+// @BasePath /
+func main() {
+	r := chi.NewRouter()
+
+	r.Get("/swagger/*", httpswagger.Handler(
+		httpswagger.URL("http://127.0.0.1:1323/swagger/doc.json"), //The url pointing to API definition
+	))
+
+	r.Handle("/echo", rest.WithLog(echo))
+
+	http.ListenAndServe(":1323", r)
+}
