2737fcd62a6259dcab06eb8b31a723aa57467a43
liuzl <liang@zliu.org>
2024-08-20 15:53:37 +0800

    telegram bot

---
+++ gemini/bot/go.mod
@@ -0,0 +1,8 @@
+module bot
+
+go 1.21.5
+
+require (
+	github.com/joho/godotenv v1.5.1
+	gopkg.in/telebot.v3 v3.3.8
+)
---
+++ gemini/bot/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/joho/godotenv/autoload"
+	tele "gopkg.in/telebot.v3"
+)
+
+func main() {
+	pref := tele.Settings{
+		Token:  os.Getenv("TOKEN"),
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+	}
+
+	b, err := tele.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Handle("/hello", func(c tele.Context) error {
+		return c.Send("Hello!")
+	})
+
+	b.Start()
+}
