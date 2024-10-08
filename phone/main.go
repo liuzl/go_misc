bf50884621ea3faaee49bde8678ded676d3bc758
liuzl <liang@zliu.org>
2024-10-08 15:07:32 +0800

    Refactor: Use phonenumbers library instead of libphonenumber
    The commit message reflects the core change made in the code: switching from the `libphonenumber` library to the `phonenumbers` library. This is concise and descriptive of the modification.

--- phone/main.go
+++ phone/main.go
@@ -3,25 +3,38 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/ttacon/libphonenumber"
 	"log"
+
+	"github.com/nyaruka/phonenumbers"
 )
 
 var (
-	loc = flag.String("loc", "US", "two letter country code")
+	loc   = flag.String("loc", "US", "two letter country code")
+	phone = flag.String("phone", "16502530000", "phone number")
 )
+
+var names = [...]string{
+	"FIXED_LINE",
+	"MOBILE",
+	"FIXED_LINE_OR_MOBILE",
+	"TOLL_FREE",
+	"PREMIUM_RATE",
+	"SHARED_COST",
+	"VOIP",
+	"PERSONAL_NUMBER",
+	"PAGER",
+	"UAN",
+	"VOICEMAIL",
+	"UNKNOWN",
+}
 
 func main() {
 	flag.Parse()
-	//number, err := libphonenumber.Parse("16502530000", "US")
 
-	number, err := libphonenumber.ParseAndKeepRawInput("16502530000", "US")
+	number, err := phonenumbers.Parse(*phone, *loc)
 	if err != nil {
 		log.Fatal(err, number)
 	}
-	cc := libphonenumber.GetCountryCodeForRegion(*loc)
-	tz, ok := libphonenumber.CountryCodeToTimeZones[cc]
-	if ok {
-		fmt.Println(tz)
-	}
+	numberType := phonenumbers.GetNumberType(number)
+	fmt.Println(names[numberType])
 }
